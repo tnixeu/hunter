043447999dcffa25b4bfa33f170d096930241137
tnixeu <4436784+tnixeu@users.noreply.github.com>
2019-10-24 20:09:48 +0200

    adds hunter_cmake_args

--- cmake/projects/nng/hunter.cmake
+++ cmake/projects/nng/hunter.cmake
@@ -5,6 +5,7 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -17,6 +18,14 @@ hunter_add_version(
     "https://github.com/nanomsg/nng/archive/v1.1.1.tar.gz"
     SHA1
     6194ae1187df0ebb2907bb17a8a724496a763bf5
+)
+
+hunter_cmake_args(
+    nng
+    CMAKE_ARGS
+        NNG_TOOLS=OFF
+        NNG_TESTS=OFF
+        NNG_ENABLE_NNGCAT=OFF
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
